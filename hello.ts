a704513eac45023154823433d7652b90e692a3a8
Peng Li <nowindlee@gmail.com>
2024-01-19 17:24:14 +0800

    improve

--- hello.ts
+++ hello.ts
@@ -14,6 +14,14 @@ async function main() {
     console.error(error)
     return 0;
   }));
+  try {
+    hello(await myPromise(true).then(x => x * 4).catch(error => {
+      console.error(error)
+      throw error;
+    }));
+  } catch (error) {
+    console.error('### catch error from outside', error);
+  }
 }
 
 main();
